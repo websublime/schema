e5c5e52bd4732a240fd5d023e1a44e0648c911f3
Hugo Rodrigues <hugorodrigues@prozis.com>
2022-02-18 16:19:29 +0000

    feat: schema validation initial code

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/next/**/*.{js,ts,tsx}", "!**/*.d.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 82,
+      statements: 80
+    }
+  },
+  globals: {
+    "ts-jest": {
+      // babelConfig: true,
+      diagnostics: false,
+      isolatedModules: true,
+      tsconfig: "<rootDir>/tsconfig.test.json"
+    }
+  },
+  moduleDirectories: ["node_modules"],
+  moduleFileExtensions: ["ts", "js", "tsx"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^lodash-es$": "lodash"
+  },
+  roots: ["<rootDir>"],
+
+  testMatch: ["<rootDir>/tests/**/*.spec.(js|jsx|ts|tsx)"],
+  transform: {
+    "\\.(ts|tsx)$": "ts-jest"
+    // '\\.(ts|tsx)$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!vue-router)",
+    "<rootDir>/node_modules/(?!(lodash-es)/.*)",
+    "dist",
+    "temp"
+  ],
+  verbose: false
+};
