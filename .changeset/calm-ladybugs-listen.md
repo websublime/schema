31ddc54d7c7272778fce8758c9e1a3eeca58724c
Miguel Ramos <miguel.marques.ramos@gmail.com>
2022-03-23 23:41:02 +0000

    feat: action to publish gh page

---
+++ .changeset/calm-ladybugs-listen.md
@@ -0,0 +1,7 @@
+---
+'@websublime/schema': minor
+---
+
+Public documentation
+
+Release and build github page for great documentation.
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/schema/',
   title: 'WebSublime Schema',
   description: 'Schema validation model',
   themeConfig: {
